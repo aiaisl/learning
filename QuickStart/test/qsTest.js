5488fac2636cd889a34b63d138f91d4370995255
luosiwei <aiaisl@outlook.com>
2015-07-29 17:14:16 +0800

    新的测试系统

--- QuickStart/test/qsTest.js
+++ QuickStart/test/qsTest.js
@@ -18,9 +18,21 @@ client
 	.init()
 	.url("http://localhost:8080/")
 	.setValue("#textArea","hello")
+	.submitForm('form')
+	.setValue("#textArea","world")
 	.submitForm('form').then(function(){
 		client.getText('body>p').then(function(value){
-			console.log(value);
+			assert(value === 'hello');
 		})
-		.end();
+		.click('#deleteNode').then(function(){
+			client.elements('body>p').then(function(elements){
+				assert(elements.value.length === 0);
+			})
+			.click('#deleteNode').then(function(){
+				client.alertText(function(err, res) {
+					assert(res === "Nothing to remove!");
+				})
+				.end();
+			})
+		})
 	})
--- webdriver/test.js
+++ webdriver/test.js
@@ -1,16 +1,23 @@
-var webdriver = require('selenium-webdriver');
+var path = require('path');
+var Nightmare = require('nightmare');
+var should = require('chai').should();
 
-var driver = new webdriver.Builder().
-   withCapabilities(webdriver.Capabilities.chrome()).
-   build();
+describe('Nightmare demo', function () {
+    this.timeout(15000); // Set timeout to 15 seconds, instead of the original 2 seconds
 
-driver.get('http://www.google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
-driver.findElement(webdriver.By.name('btnG')).click();
-driver.wait(function() {
- return driver.getTitle().then(function(title) {
-   return title === 'webdriver - Google Search';
- });
-}, 1);
+    var url = 'http://localhost/ngy2';
 
-driver.quit();
+    describe('首页', function () {
+        it('首页标题等于用户名', function (done) {
+            new Nightmare({weak:false})
+                .goto(url)
+                .evaluate(function () {
+                    return $('.hyzx h2').text();
+                }, function (result) {
+                    result.should.equal("行业资讯");
+                    done();
+                })
+                .run();
+        });
+    });
+});

701b81bbe95c5a0330a878acf9586e10951c3b07
luosiwei <aiaisl@outlook.com>
2015-08-08 10:07:34 +0800

    1

--- webdriver/test.js
+++ webdriver/test.js
@@ -1,23 +1,24 @@
 var path = require('path');
 var Nightmare = require('nightmare');
 var should = require('chai').should();
-
 describe('Nightmare demo', function () {
     this.timeout(15000); // Set timeout to 15 seconds, instead of the original 2 seconds
-
-    var url = 'http://localhost/ngy2';
-
-    describe('首页', function () {
+    var url = 'http://localhost:8080/Learning-jQuery/chapter10/';
         it('首页标题等于用户名', function (done) {
             new Nightmare({weak:false})
+                .on('alert', function(msg){
+                    console.log(msg);
+                    done();
+                })
                 .goto(url)
                 .evaluate(function () {
-                    return $('.hyzx h2').text();
+                    return $('h1').text();
                 }, function (result) {
-                    result.should.equal("行业资讯");
-                    done();
+                    result.should.equal("Photo Gallery");
+                })
+                .title(function(result){
+                    console.log(result);
                 })
                 .run();
         });
-    });
 });

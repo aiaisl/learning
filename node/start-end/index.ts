57aeab145910d450f3ecc6554f4d20e83f8d361b
luosiwei <aiaisl@outlook.com>
2015-08-31 10:59:46 +0800

    使用Q模块

--- node/start-end/index.ts
+++ node/start-end/index.ts
@@ -1,6 +1,24 @@
 /// <reference path="../../typings/node/node.d.ts" />
+/// <reference path="../../typings/q/q.d.ts" />
 
-var _ = require('underscore');
-_.each([1, 2, 3], function(num){
-	console.log("underscore.js says " + num);
+import fs = require("fs");
+import q = require("q");
+
+var fsReadFile_deferd = function(file:string, encoding?: string):q.Promise<{}> {
+	var deferred = q.defer();
+	fs.readFile(file, encoding, function(error, result){
+		if(error) {
+			deferred.reject(error.toString());
+		}
+		deferred.resolve(result);
+	});
+	return deferred.promise;
+}
+
+fsReadFile_deferd('./index.js').then(function(result){
+	console.log("invoke in deferd");
+	console.log(result.toString());
+}, function(error){
+	console.log("invoke in deferd");
+	console.log(error.toString());
 });

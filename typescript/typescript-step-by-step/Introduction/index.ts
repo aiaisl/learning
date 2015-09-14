95e2f33ff513fb91ae5f34684e4d552406de696f
luosiwei <aiaisl@outlook.com>
2015-09-14 14:24:57 +0800

    Ts中的对象是基于接口的,我们可以利用此功能来增强Ts的代码提示功能

--- typescript/typescript-step-by-step/Introduction/index.ts
+++ typescript/typescript-step-by-step/Introduction/index.ts
@@ -16,3 +16,42 @@ vote('BigPig', function(resule: string){
 		
 	}
 })
+
+interface Friend{
+	name: string;
+	favoriteColor?: string;
+}
+
+function add(friend: Friend) {
+	var name = friend.name;
+}
+
+var fr: Friend = {
+	name: "Jill",
+	favoriteColor: "green"
+}
+
+add({name:"Fred"});
+add(fr);
+add({favoriteColor: "blue"});//Error
+
+interface JQuery {
+	text(content: string);
+}
+
+interface JQueryStatic {
+	get(url: string, callback: (data: string) => any);
+	(query: string): JQuery;
+}
+
+declare var $: JQueryStatic;
+
+$.get("http://mysite.org/divContent",function(data: string){
+	$("div").text(data);
+})
+
+var fi: {
+	(): string;
+};
+
+var sameType: () => string = fi;

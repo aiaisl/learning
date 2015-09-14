e3c8b40e7759a727a090c2f850a87ba728140610
luosiwei <aiaisl@outlook.com>
2015-09-14 14:10:30 +0800

    可以在声明函数时制定参数类型和返回值类型.函数也是一种数据类型

---
+++ typescript/typescript-step-by-step/Introduction/index.js
@@ -0,0 +1,12 @@
+function f(s) {
+    var i = "hello";
+    return i + s;
+}
+console.log(f('world').length);
+function vote(candidate, callback) {
+}
+vote('BigPig', function (resule) {
+    if (resule === "BigPib") {
+    }
+});
+//# sourceMappingURL=index.js.map
--- typescript/typescript-step-by-step/Introduction/index.ts
+++ typescript/typescript-step-by-step/Introduction/index.ts
@@ -5,3 +5,14 @@ function f(s: string){
 	return i + s;
 }
 console.log(f('world').length);
+
+
+function vote(candidate: string, callback: (resule: string)=> any){
+	
+}
+
+vote('BigPig', function(resule: string){
+	if(resule === "BigPib"){
+		
+	}
+})

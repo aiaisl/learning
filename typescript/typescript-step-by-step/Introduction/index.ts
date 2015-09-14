155ff42db991a4aec175d70fae89fe2f2c636088
luosiwei <aiaisl@outlook.com>
2015-09-14 14:30:35 +0800

    Ts的代码是基于编译型的所以真正的接口是不存在的. Ts中的接口是直接基于对象的值.

--- typescript/typescript-step-by-step/Introduction/index.ts
+++ typescript/typescript-step-by-step/Introduction/index.ts
@@ -55,3 +55,27 @@ var fi: {
 };
 
 var sameType: () => string = fi;
+
+interface Point{
+	x: number;
+	y: number;
+}
+
+function getX(p: Point){
+	return p.x;
+}
+
+class CPoint{
+	x: number;
+	y: number;
+	constructor(x: number, y: number){
+		this.x = x;
+		this.y = y;
+	}
+}
+
+getX(new CPoint(0, 0))
+
+getX({x:0, y:0, color:"red"});
+
+getX({x: 0});//Error

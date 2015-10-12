0ef535b8224b615c4a27e7cd3db1023ac0824c89
luosiwei <aiaisl@outlook.com>
2015-10-12 17:28:26 +0800

    测试驱动开发的艺术项目

---
+++ TDDFORCSHARP/TDDFORCSHARP/Template.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace TDDFORCSHARP
+{
+    class Template
+    {
+        private String templateText;
+
+        private Hashtable variables;
+
+        public Template(string templateText)
+        {
+            variables = new Hashtable();
+            this.templateText = templateText;
+        }
+
+        public string evaluate()
+        {
+            string result = templateText;
+            foreach (DictionaryEntry de in variables)
+            {
+                string regex = "${" + de.Key.ToString() + "}";
+                result = result.Replace(regex, de.Value.ToString());
+            }
+            return result;
+        }
+
+        public void set(string name, string value)
+        {
+            variables.Add(name, value);
+        }
+    }
+}
--- typescript/typescript-step-by-step/Introduction/index.ts
+++ typescript/typescript-step-by-step/Introduction/index.ts
@@ -1,23 +1,23 @@
 declare var jquery;
 
-function f(s: string){
+function f(s: string) {
 	var i = "hello";
 	return i + s;
 }
 console.log(f('world').length);
 
 
-function vote(candidate: string, callback: (resule: string)=> any){
-	
+function vote(candidate: string, callback: (resule: string) => any) {
+
 }
 
-vote('BigPig', function(resule: string){
-	if(resule === "BigPib"){
-		
+vote('BigPig', function(resule: string) {
+	if (resule === "BigPib") {
+
 	}
 })
 
-interface Friend{
+interface Friend {
 	name: string;
 	favoriteColor?: string;
 }
@@ -31,9 +31,9 @@ var fr: Friend = {
 	favoriteColor: "green"
 }
 
-add({name:"Fred"});
+add({ name: "Fred" });
 add(fr);
-add({favoriteColor: "blue"});//Error
+add({ favoriteColor: "blue" });//Error
 
 interface JQuery {
 	text(content: string);
@@ -46,7 +46,7 @@ interface JQueryStatic {
 
 declare var $: JQueryStatic;
 
-$.get("http://mysite.org/divContent",function(data: string){
+$.get("http://mysite.org/divContent", function(data: string) {
 	$("div").text(data);
 })
 
@@ -56,19 +56,19 @@ var fi: {
 
 var sameType: () => string = fi;
 
-interface Point{
+interface Point {
 	x: number;
 	y: number;
 }
 
-function getX(p: Point){
+function getX(p: Point) {
 	return p.x;
 }
 
-class CPoint{
+class CPoint {
 	x: number;
 	y: number;
-	constructor(x: number, y: number){
+	constructor(x: number, y: number) {
 		this.x = x;
 		this.y = y;
 	}
@@ -76,10 +76,77 @@ class CPoint{
 
 getX(new CPoint(0, 0))
 
-getX({x:0, y:0, color:"red"});
+getX({ x: 0, y: 0, color: "red" });
 
-getX({x: 0});//Error
+getX({ x: 0 });//Error
 
-function mal(a: number, b: number){
+function mal(a: number, b: number) {
 	return a * b;
 }
+
+class BankAccount {
+	balance = 0;
+	constructor(initially: number) {
+		this.balance = initially;
+	}
+	deposit(credit: number) {
+		this.balance += credit;
+		return this.balance;
+
+	}
+}
+
+class CheckingAccount extends BankAccount {
+	constructor(balance: number) {
+		super(balance);
+	}
+	writeCheck(debit: number) {
+		this.balance -= debit;
+	}
+}
+
+class Accessor {
+
+}
+function attr(name: string): string;
+function attr(name: string, value: string): Accessor;
+function attr(map: any): Accessor;
+function attr(nameOrMap: any = "wind", value?: string): any {
+	if (nameOrMap && typeof nameOrMap === "string") {
+
+	} else {
+
+	}
+}
+attr("myName", "hi");
+
+interface Comparable {
+	localeCompare(other: any): number;
+}
+
+function compare<T extends Comparable>(x: T, y: T): number {
+	if (x == null) {
+		return y == null ? 0 : -1;
+	}
+	if(y == null) {
+		return 1;
+	}
+	return x.localeCompare(y);
+}
+
+class Messager {
+	message = "Hello World";
+	constructor();
+	constructor(message: string)
+	constructor(message: string = "a") {
+		this.message = message;
+	}
+	start() {
+		setTimeout(()=>{
+			alert(this.message);
+		}, 3000);
+	}
+}
+var message = new Messager();
+
+message.start();
